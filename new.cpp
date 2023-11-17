703d771726bda15ec251e62919a4c3e5695ccd5e
Cl0uss <kasradze.niko@gmail.com>
2023-11-17 19:24:00 +0400

    changed

--- new.cpp
+++ new.cpp
@@ -1,9 +1,31 @@
 #include <iostream>
 #include <unistd.h>
+#include <cstdlib>
+#include <ctime>
 #include <string>
+#include <random>
 using namespace std;
+void game(string targetWord)
+{
+string userWord;
+cout<<"Write your guess: ";
+cin>>userWord;
+if (userWord.size()!=5)
+{
+cout<<"Wrong length!\n";
+return game(targetWord);
+}
+//continue from here; need to write double for loop to check for getting same letters
+}
 int main()
 {
+srand(time(NULL));
+//
+random_device rd;
+mt19937 gen(rd());
+uniform_int_distribution<> dis(0,170);
+int random=dis(gen);
+//random number generator
 string words[] = {"apple", "happy", "smile", "table", "story", "chair", "cloud", "green", "funny", "music", "dance", "puppy", "beach", "magic",
 "color", "bunny", "swing", "robot", "fairy", "jelly", "sunny", "snack", "lucky", "dream", "giant", "ocean", "silly", "zebra", "daisy", "laser",
  "flame", "honey","river", "sugar", "dream", "cheer", "laugh", "chase", "spark", "spark", "happy", "globe", "melon", "peace", "cloud", "smile",
@@ -15,5 +37,5 @@ string words[] = {"apple", "happy", "smile", "table", "story", "chair", "cloud",
  "girly", "happy","inbox", "juice", "kitty", "lucky", "mango", "novel", "oasis", "puppy", "quack", "robin", "sunny", "tiger", "umbra", "vivid",
  "whale", "yummy", "zebra", "apple","bunny", "comic", "dance", "elbow", "fairy", "girly", "happy", "inbox", "juice", "kitty", "lucky", "mango",
  "novel", "oasis", "puppy", "quack","robin", "sunny", "tiger", "umbra", "vivid", "whale", "yummy", "zebra"};
-cout<<sizeof(words)/sizeof(words[0]);
+string targetWord=words[random];
 }
