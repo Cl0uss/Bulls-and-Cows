630837a4d109d841bf05991d812dbea3559083c1
Cl0uss <kasradze.niko@gmail.com>
2023-11-17 19:01:10 +0400

    Linux version

---
+++ new.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <unistd.h>
+#include <string>
+using namespace std;
+int main()
+{
+string words[] = {"apple", "happy", "smile", "table", "story", "chair", "cloud", "green", "funny", "music", "dance", "puppy", "beach", "magic",
+"color", "bunny", "swing", "robot", "fairy", "jelly", "sunny", "snack", "lucky", "dream", "giant", "ocean", "silly", "zebra", "daisy", "laser",
+ "flame", "honey","river", "sugar", "dream", "cheer", "laugh", "chase", "spark", "spark", "happy", "globe", "melon", "peace", "cloud", "smile",
+ "sweet", "peace","chirp", "candy", "frost", "grape", "heart", "happy", "jolly", "kitty", "light", "magic", "novel", "oasis", "peace", "quack",
+ "robot", "silly","tiger", "upend", "vivid", "wings", "yummy", "zooey", "apple", "bunny", "comic", "dance", "elbow", "fairy", "girly", "happy",
+ "inbox", "juice","kitty", "lucky", "mango", "novel", "oasis", "puppy", "quack", "robin", "sunny", "tiger", "umbra", "vivid", "whale", "yummy",
+ "zebra", "abide","blink", "candy", "daisy", "elbow", "frost", "giant", "happy", "inbox", "jolly", "kitty", "laser", "melon", "novel", "oasis",
+ "peace", "quack","robot", "silly", "tiger", "umbra", "vivid", "wings", "yummy", "zebra", "agile", "bunny", "comic", "dance", "elbow", "fairy",
+ "girly", "happy","inbox", "juice", "kitty", "lucky", "mango", "novel", "oasis", "puppy", "quack", "robin", "sunny", "tiger", "umbra", "vivid",
+ "whale", "yummy", "zebra", "apple","bunny", "comic", "dance", "elbow", "fairy", "girly", "happy", "inbox", "juice", "kitty", "lucky", "mango",
+ "novel", "oasis", "puppy", "quack","robin", "sunny", "tiger", "umbra", "vivid", "whale", "yummy", "zebra"};
+cout<<sizeof(words)/sizeof(words[0]);
+}
